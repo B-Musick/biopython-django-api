36ffc4d007aa80656db2389847a19d8ef77b0708
Brendan Musick <bmuze1@gmail.com>
2024-05-30 08:31:58 -0600

    #7 - added tests for posting fasta file

--- file_upload_api/tests.py
+++ file_upload_api/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse;
+from rest_framework.test import APITestCase, APIClient
+import os
+module_dir = os.path.dirname(__file__)  # get current directory
 
-# Create your tests here.
+class EntrezTests(APITestCase):
+    def setUp(self):
+        self.upload_url = reverse('upload')
+
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+
+    def test_upload_fasta_response(self):
+        file_path = os.path.join(module_dir, "public/sequence.fasta")
+
+        response = ''
+
+        with open(file_path) as file:
+            response = self.client.post(self.upload_url, {"fileType":"fasta", "file":file})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(list(response.data)[0], 'records')
+        self.assertEqual(len(response.data), 1)
+        
+        record = response.data["records"][0]
+
+        self.assertIn("id", record)
+        self.assertIn("features", record)
+        self.assertIn("annotations", record)
+        self.assertIn("seq", record)
+        self.assertIn("name", record)
+        self.assertIn("description", record)
+        self.assertIn("dbxrefs", record)
+        self.assertIn("letter_annotations", record)
+
+        self.assertEqual("NM_005546.3", record["id"])
